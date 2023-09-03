22fac6ce8f41bbc0845733dd75656122fbbcfc19
Ezro <justinmvalentin@gmail.com>
2023-09-02 22:42:12 -0600

    Update the code

--- skeleton-frontend/src/routes/notes/+page.svelte
+++ skeleton-frontend/src/routes/notes/+page.svelte
@@ -1,192 +1,179 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { modalStore, type ModalSettings } from '@skeletonlabs/skeleton';
+  import { onMount } from 'svelte'
+  import { modalStore, type ModalSettings } from '@skeletonlabs/skeleton'
+  import { ImageIcon, Trash2Icon } from 'svelte-feather-icons'
 
-	let takingNote = false;
-	let noteTitle = '';
-	let noteInputValue = '';
-	let noteInput: HTMLInputElement;
-	let notes: any[] = [];
+  let takingNote = false
+  let noteTitle = ''
+  let noteInputValue = ''
+  let noteInput: HTMLInputElement
+  let notes = {}
+  let noteId = 0
+  let noteImages: FileList
 
-	onMount(() => {
-		// console.log('notes page');
-	});
+  onMount(() => {
+    // console.log('notes page');
+  })
 
-	function openNote() {
-		const modal: ModalSettings = {
-			type: 'prompt',
-			title: 'Example Note',
-			body: 'This is an example note.',
-			image: 'https://i.imgur.com/WOgTG96.gif'
-		};
-		modalStore.trigger(modal);
-	}
+  function openNote(event: MouseEvent) {
+    if (event.target?.closest('.card-footer')) {
+      return
+    }
+    const modal: ModalSettings = {
+      type: 'prompt',
+      title: 'Example Note',
+      body: 'This is an example note.',
+      image: 'https://i.imgur.com/WOgTG96.gif'
+    }
+    modalStore.trigger(modal)
+  }
 
-	function documentClick(event: MouseEvent) {
-		if (event.target.parentNode.id !== 'notetaker') {
-			createNote();
-			// console.log(noteTitle, noteInputValue);
-		}
-	}
+  function documentClick(event: MouseEvent) {
+    if (event.target?.parentNode.id !== 'notetaker') {
+      createNote()
+    }
+  }
 
-	function openNoteTaking() {
-		takingNote = true;
-		setTimeout(() => {
-			noteInput.focus();
-		});
-	}
+  function openNoteTaking() {
+    takingNote = true
+    setTimeout(() => {
+      noteInput.focus()
+    })
+  }
 
-	function createNote() {
-		if (noteTitle === '' && noteInputValue === '') {
-			return;
-		}
-		console.log(noteTitle, noteInputValue);
-		takingNote = false;
-		let note = {
-			title: noteTitle,
-			createTime: Date.now(),
-			body: noteInputValue
-		};
-		notes.push(note);
-		notes = notes;
-		noteTitle = '';
-		noteInputValue = '';
-		console.log(notes);
-	}
+  function createNote() {
+    takingNote = false
+    if (noteTitle === '' && noteInputValue === '') {
+      return
+    }
+    let note = {
+      title: noteTitle,
+      createTime: Date.now(),
+      body: noteInputValue
+    }
+    notes[noteId] = note
+    notes = notes
+    noteId += 1
+    noteTitle = ''
+    noteInputValue = ''
+  }
 
-	//   {
-	//     "name": string,
-	//     "createTime": string,
-	//     "updateTime": string,
-	//     "trashTime": string,
-	//     "trashed": boolean,
-	//     "attachments": [
-	//         {
-	//             object(Attachment)
-	//         }
-	//     ],
-	//     "permissions": [
-	//         {
-	//             object(Permission)
-	//         }
-	//     ],
-	//     "title": string,
-	//     "body":
-	//     {
-	//         object(Section)
-	//     }
-	//   }
+  function deleteNote(noteIdStr: string) {
+    noteId = parseInt(noteIdStr)
+    delete notes[noteId]
+    notes = notes
+  }
+
+  //   {
+  //     "name": string,
+  //     "createTime": string,
+  //     "updateTime": string,
+  //     "trashTime": string,
+  //     "trashed": boolean,
+  //     "attachments": [
+  //         {
+  //             object(Attachment)
+  //         }
+  //     ],
+  //     "permissions": [
+  //         {
+  //             object(Permission)
+  //         }
+  //     ],
+  //     "title": string,
+  //     "body":
+  //     {
+  //         object(Section)
+  //     }
+  //   }
 </script>
 
 <svelte:window on:mousedown={documentClick} />
 
 <!-- svelte-ignore a11y-click-events-have-key-events -->
 {#if !takingNote}
-	<div class="card note-creator" on:click={openNoteTaking}>
-		<input placeholder="Take a note..." />
-	</div>
+  <div class="card note-creator" on:click={openNoteTaking}>
+    <div class="p-4 space-y-4">
+      <input placeholder="Take a note..." />
+    </div>
+  </div>
 {:else}
-	<div id="notetaker" class="card note-creator">
-		<form id="notetaker" on:submit|preventDefault={createNote}>
-			<input bind:value={noteTitle} placeholder="Title" />
-		</form>
-		<form id="notetaker" on:submit|preventDefault={createNote}>
-			<input
-				bind:this={noteInput}
-				bind:value={noteInputValue}
-				placeholder="Take a note..."
-				on:submit={createNote}
-			/>
-		</form>
-	</div>
+  <div id="notetaker" class="card note-creator">
+    <div id="notetaker" class="p-4 space-y-4">
+      <form id="notetaker" on:submit|preventDefault={createNote}>
+        <input bind:value={noteTitle} placeholder="Title" />
+      </form>
+      <form id="notetaker" on:submit|preventDefault={createNote}>
+        <input
+          bind:this={noteInput}
+          bind:value={noteInputValue}
+          placeholder="Take a note..."
+          on:submit={createNote}
+        />
+      </form>
+    </div>
+    <div class="card-footer create-note-footer">
+      <form style="width: 18px; padding-top: 15px">
+        <ImageIcon size="18" />
+      </form>
+      <form style="width: 18px; padding-top: 15px">
+        <Trash2Icon size="18" />
+      </form>
+    </div>
+  </div>
 {/if}
 <div class="note-grid">
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div class="note card card-hover" on:click={openNote}>
-		<header>
-			<img
-				src="https://images.unsplash.com/photo-1620562303747-ba21ca6f1530?ixid=M3w0Njc5ODF8MHwxfGFsbHx8fHx8fHx8fDE2ODc5NzY2MjR8&ixlib=rb-4.0.3&w=400&h=175&auto=format&fit=crop"
-				alt="Vibrant sombrero skeleton with a guitar"
-			/>
-		</header>
-		<div class="p-4 space-y-4">
-			<h6 class="h6" data-toc-ignore>Announcements</h6>
-			<h3 class="h3" data-toc-ignore>Skeleton is Awesome!</h3>
-			<article>
-				<p>
-					<!-- cspell:disable -->
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet
-					eligendi cumque consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias
-					cum expedita architecto itaque quae rem.
-					<!-- cspell:enable -->
-				</p>
-			</article>
-		</div>
-		<hr class="opacity-50" />
-		<footer class="p-4 flex justify-start items-center space-x-4">
-			<div class="flex-auto flex justify-between items-center">
-				<h6 class="font-bold" data-toc-ignore>By Alex</h6>
-				<small>On {new Date().toLocaleDateString()}</small>
-			</div>
-		</footer>
-	</div>
-
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div class="note card card-hover" on:click={openNote}>
-		<div class="p-4 space-y-4">
-			<h6 class="h6" data-toc-ignore>Announcements</h6>
-			<h3 class="h3" data-toc-ignore>Skeleton is Awesome!</h3>
-			<article>
-				<p>
-					<!-- cspell:disable -->
-					Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam aspernatur provident eveniet
-					eligendi cumque consequatur tempore sint nisi sapiente. Iste beatae laboriosam iure molestias
-					cum expedita architecto itaque quae rem.
-					<!-- cspell:enable -->
-				</p>
-			</article>
-		</div>
-	</div>
-	<!-- svelte-ignore a11y-click-events-have-key-events -->
-	<div class="note card card-hover" on:click={openNote}>
-		<div class="p-4 space-y-4">
-			<article>
-				<p>Hi</p>
-			</article>
-		</div>
-	</div>
-	{#each notes as note, i}
-		<div class="note card card-hover" on:click={openNote}>
-			{#if note.title}
-				<div class="card-header">
-					<h6 class="h6" data-toc-ignore>{note.title}</h6>
-				</div>
-			{/if}
-			<div class="p-4 space-y-4">
-				<article>
-					<p>
-						<!-- cspell:disable -->
-						{note.body}
-						<!-- cspell:enable -->
-					</p>
-				</article>
-			</div>
-		</div>
-	{/each}
+  {#each Object.entries(notes).reverse() as [noteId, note]}
+    <!-- svelte-ignore a11y-click-events-have-key-events -->
+    <div
+      class="note card card-hover"
+      on:click={() => {
+        openNote(noteId)
+      }}
+    >
+      {#if note.title}
+        <div class="card-header">
+          <h6 class="h6" data-toc-ignore>{note.title}</h6>
+        </div>
+      {/if}
+      <div class="p-4 space-y-4">
+        <article>
+          <p>
+            <!-- cspell:disable -->
+            {note.body}
+            <!-- cspell:enable -->
+          </p>
+        </article>
+      </div>
+      <div class="card card-footer">
+        <form
+          on:click={() => {
+            deleteNote(noteId)
+          }}
+          style="width: 18px; padding-top: 15px"
+        >
+          <Trash2Icon size="18" />
+        </form>
+      </div>
+    </div>
+  {/each}
 </div>
 
 <style>
-	.note-creator {
-		display: flex;
-		margin: 2rem;
-		max-width: 20rem;
-		align-content: center;
-	}
-	.note-grid {
-		display: grid;
-		gap: 1rem;
-		grid-template-columns: repeat(auto-fill, minmax(min(10rem, 100%), 1fr));
-		margin: 2rem;
-		align-items: flex-start;
-	}
+  .note-creator {
+    display: flex;
+    margin: 2rem;
+    max-width: 20rem;
+    align-content: center;
+  }
+  .create-note-footer {
+    display: flex;
+  }
+  .note-grid {
+    display: grid;
+    gap: 1rem;
+    grid-template-columns: repeat(auto-fill, minmax(min(10rem, 100%), 1fr));
+    margin: 2rem;
+    align-items: flex-start;
+  }
 </style>
